3199b3578129cd76b816e8adc3835c4fb87f228c
Ura20 <ura2091.212@gmail.com>
2024-11-19 22:06:22 +0900

    feat:４−８課題

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,16 @@
 <?php
+echo "<b>4-8</b><br/>";
+echo "課題１：";
 
+$total = 0;
+for($i=0; $i<5; $i++){
+    $total += $i;
+    var_dump($total);
+}
+
+echo '<br/>課題２：';
+$total = 0;
+for ($i = 0; $i < ($total = 105); $i++) {
+    $total += $i;
+}
+var_dump($total);
